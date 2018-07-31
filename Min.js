d102c7136e0cb9d7cc397648386523d2e2a2e852
student <royzellwalton@gmail.com>
2018-07-31 14:23:13 -0500

    Number 17 is done.

---
+++ Min.js
@@ -0,0 +1,16 @@
+function main() {
+
+    var num1 = parseInt(2);
+    var num2 = parseInt(2);
+    var num3 = parseInt(2);
+    sumNum(num1, num2, num3);
+}
+
+
+main();
+
+function sumNum(num1, num2, num3) {
+    var sumOfNumbers = num1 + num2 + num3;
+    alert(sumOfNumbers);
+
+}
