01aacbf07fa39c506ce4a18c1c2c51e7c6424615
student <royzellwalton@gmail.com>
2018-07-31 14:27:07 -0500

    Number 18 done.

--- Min.js
+++ Min.js
@@ -4,6 +4,10 @@ function main() {
     var num2 = parseInt(2);
     var num3 = parseInt(2);
     sumNum(num1, num2, num3);
+
+    var num4 = parseInt(2);
+    var num5 = parseInt(2);
+    alert(product(num4, num5));
 }
 
 
@@ -12,5 +16,11 @@ main();
 function sumNum(num1, num2, num3) {
     var sumOfNumbers = num1 + num2 + num3;
     alert(sumOfNumbers);
+
+}
+
+function product(num4, num5) {
+    var multiples = num4 * num5;
+    return(multiples);
 
 }
